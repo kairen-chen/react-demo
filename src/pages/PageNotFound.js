f836645bb0fe11c57fcf20b534a342a2b6dde83f
Kairen <kairen.chen@incrte.com>
2021-04-30 15:07:51 +0800

    換色搞定

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import styled,{ThemeProvider } from "styled-components";
+import styled, {ThemeProvider, css} from "styled-components";
 import React,
   {
     useState
@@ -7,58 +7,68 @@ import React,
 from "react";
 import { useHistory } from "react-router";
 
+
+
 const 
   Container = styled.div`
     border: 1px solid red;
     margin: 40px 0;
     fontsize: 20px;
-
     p {
       color:${props => props.theme.themeColor};
     }
+
+    // 淺色主題
+    ${ props => props.main && css`
+      border-width: 2px 3px 2px 5px;
+      border-radius: 90% 6% 93% 5% / 5% 94% 7% 95%;
+      transform: rotate(2deg);
+      color: black;
+      background-color: white;
+    `}
   `;
 
 Container.defaultProps = {
+  main: "",
   theme : {
     themeColor: "palevioletred"
   }
 }
 
 
-export default function PageNotFound(props) {
+export default function PageNotFound( props ) {
   // console.log(props)
-  const [theme = {}, setTheme] = useState( Container.defaultProps.theme );
-  const history = useHistory();
-  useEffect(()=>{
+  const [theme = {}, setTheme] = useState( Container.defaultProps.theme ),
+        [main, setMain] =  useState( Container.defaultProps.main ),
+        history = useHistory();
+  useEffect( ()=>{
     window.previousLocation = props.location;
- 
-    // 達到某條件換色
-    // setTheme(()=>{ return {main : "cornflowerblue"} })
 
     // history.push({
     //   pathname:  "/",
     // });
-  },[props.location,history])
+  },[ props.location, history ] )
   
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <label htmlFor="changeTheme"> 換膚 </label>
+    <ThemeProvider theme = { theme }>
+      <Container main = { main }>
+        Sorry,查無此路 ->
+        <p>✨ {window.location.href} ✨</p>
+        <br/>
+        <label htmlFor = "changeTheme"> "都來這了,點我試試" </label>
         <input
-          type="checkbox"
-          id="changeTheme"  
+          type = "checkbox"
+          id = "changeTheme"
+          hidden
           onChange = {
-            (e)=>{
-              setTheme(()=>{
-                return {themeColor : (!e.target.checked ? Container.defaultProps.theme.themeColor : "cornflowerblue")}
-              })
+            ( e )=>{
+              setTheme( ()=>{
+                setMain( () => e.target.checked ? "light" : "")
+                return { themeColor : (!e.target.checked ? Container.defaultProps.theme.themeColor : "cornflowerblue") }
+              } )
             }
           } 
         />
-        <br/>
-        你確定有這條路 ?? 
-        <p>✨ {window.location.href}</p>
-        <p>順便展示更換主題色</p>
       </Container>
     </ThemeProvider>
   );
