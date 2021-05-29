6ee6a87e5fe598e4e98e37f731b29da772ab3b84
kairen <s96113123@gmail.com>
2021-05-29 20:15:53 +0800

    CD test

--- config/paths.js
+++ config/paths.js
@@ -52,7 +52,8 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  // appBuild: resolveApp('build'),
+  appBuild: resolveApp('BaseUrlDemo'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/entry'),
